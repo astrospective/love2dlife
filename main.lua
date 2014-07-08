41592cb457eef2627a0db5b9f8ca526af0ee7f00
astrospective <eric.dalehite@gmail.com>
2014-07-07 17:46:43 -0700

    fixed the individual row updating, added a conf.lua file
    Reworked the individual row/column updating to work off of whatever
    value I pass instead of only going up to 40. Added the conf.lua file.
    Added a stub for a main menu.

--- main.lua
+++ main.lua
@@ -55,10 +55,10 @@ Grid = Class{
 			for j=1,self.y,1 do
 				if(self.mt[i][j]:getState() == 0) then
 					love.graphics.setColor (255, 255, 255)
-					love.graphics.rectangle("fill", i*20, j*20, 20, 20)
+					love.graphics.rectangle("fill", (i*10), (j*10), 8, 8)
 				elseif(self.mt[i][j]:getState() == 1) then
 					love.graphics.setColor (0,0,0)
-					love.graphics.rectangle("fill", i*20, j*20, 20, 20)
+					love.graphics.rectangle("fill", (i*10), (j*10), 8, 8)
 				end
 			end
 		end
@@ -84,38 +84,38 @@ Grid = Class{
 		count = count + self.mt[self.x-1][self.y-1]:getLastState() + self.mt[self.x-1][self.y]:getLastState() + self.mt[self.x][self.y-1]:getLastState()
 		self.mt[self.x][self.y]:generate(count)
 		--do the rest
-		--first row
-		for	i=0,37,1 do
+		--first column
+		for	i=1,self.y-2,1 do
 			count = 0
 			--count = count + self.mt[1][1+i]:getLastState() + self.mt[2][1+i]:getLastState() + self.mt[2][2+i]getLastState() + self.mt[1][3+i]:getLastState() + self.mt[2][3+i]getLastState()
-			count = count + self.mt[1][1+i]:getLastState()
+			count = count + self.mt[1][i]:getLastState()
+			count = count + self.mt[2][i]:getLastState()
 			count = count + self.mt[2][1+i]:getLastState()
+			count = count + self.mt[1][2+i]:getLastState()
 			count = count + self.mt[2][2+i]:getLastState()
-			count = count + self.mt[1][3+i]:getLastState()
-			count = count + self.mt[2][3+i]:getLastState()
-			self.mt[1][2+i]:generate(count)
+			self.mt[1][1+i]:generate(count)
 		end
-		--first column
-		for i=0,37,1 do
+		--first row
+		for i=1,self.y-2,1 do
 			count = 0
-			count = count + self.mt[1+i][1]:getLastState() 
-			count = count + self.mt[3+i][1]:getLastState() 
-			count = count + self.mt[1+1][2]:getLastState()
+			count = count + self.mt[i][1]:getLastState() 
+			count = count + self.mt[2+i][1]:getLastState() 
+			count = count + self.mt[i][2]:getLastState()
+			count = count + self.mt[1+i][2]:getLastState()
 			count = count + self.mt[2+i][2]:getLastState()
-			count = count + self.mt[3+i][2]:getLastState()
-			self.mt[2+i][1]:generate(count)
+			self.mt[1+i][1]:generate(count)
 		end
 		--last row
-		for i=0,37,1 do
+		for i=1,self.y-2,1 do
 			count = 0
-			count = count + self.mt[self.x-1][1+i]:getLastState() + self.mt[self.x-1][2+i]:getLastState() + self.mt[self.x-1][3+i]:getLastState() + self.mt[self.x][1+i]:getLastState() + self.mt[self.x][3+i]:getLastState()
-			self.mt[self.x][2+i]:generate(count)
+			count = count + self.mt[self.x-1][i]:getLastState() + self.mt[self.x-1][1+i]:getLastState() + self.mt[self.x-1][2+i]:getLastState() + self.mt[self.x][i]:getLastState() + self.mt[self.x][2+i]:getLastState()
+			self.mt[self.x][1+i]:generate(count)
 		end
 		--last column
-		for i=0,37,1 do
+		for i=1,self.x-2,1 do
 			count = 0
-			count = count + self.mt[1+i][self.y-1]:getLastState() + self.mt[2+i][self.y-1]:getLastState() + self.mt[3+i][self.y-1]:getLastState() + self.mt[1+i][self.y]:getLastState() self.mt[3+i][self.y]:getLastState()
-			self.mt[2+i][self.y]:generate(count)
+			count = count + self.mt[i][self.y-1]:getLastState() + self.mt[1+i][self.y-1]:getLastState() + self.mt[2+i][self.y-1]:getLastState() + self.mt[i][self.y]:getLastState() self.mt[2+i][self.y]:getLastState()
+			self.mt[1+i][self.y]:generate(count)
 		end
 		for i=2,self.x-1 do
 			for j=2,self.y-1 do
@@ -134,12 +134,12 @@ Grid = Class{
 	return(1)
 	end;
 	check = function(self,x,y)
-		if x >= 20 and x <= (self.x * 20) then
-			if y >= 20 and y <= (self.y * 20) then
-				if self.mt[math.floor(x/20)][math.floor(y/20)]:getState() == 0 then	
-					self.mt[math.floor(x/20)][math.floor(y/20)]:setState(1)
+		if x >= 10 and x <= (self.x * 10) then
+			if y >= 10 and y <= (self.y * 10) then
+				if self.mt[math.floor(x/10)][math.floor(y/10)]:getState() == 0 then	
+					self.mt[math.floor(x/10)][math.floor(y/10)]:setState(1)
 				else
-					self.mt[math.floor(x/20)][math.floor(y/20)]:setState(0)
+					self.mt[math.floor(x/10)][math.floor(y/10)]:setState(0)
 				end
 			end
 		end
@@ -148,6 +148,7 @@ Grid = Class{
 
 local running = {}
 local paused = {}
+local mainMenu = {}
 function paused:init()
 end
 
@@ -157,7 +158,7 @@ function paused:keyreleased(key)
 	end
 end
 function paused:draw()
-	love.graphics.print("Press up to begin,click to toggle",10,8)
+	love.graphics.print("Press up to begin,click to toggle",10,820)
 	lifeLand:draw()
 end
 
@@ -177,20 +178,20 @@ function paused:mousepressed(x,y, mouse_btn)
 	end
 end
 function running:draw()
-	love.graphics.print("Press Down to Pause",10,8)
+	love.graphics.print("Press Down to Pause",10,820)
 	lifeLand:draw()
 end
 function love.load()
 	Gamestate.registerEvents()
-	love.window.setMode (840, 840)
-	lifeLand = Grid(40,40)
+	--love.window.setMode (840, 840)
+	lifeLand = Grid(80,80)
 	Gamestate.switch(paused)
 	
 end
 time = 0
 function running:update(dt)
   time = time + dt
-  if time >= 1 then
+  if time >= .25 then
     time = 0
     lifeLand:update()
   end
